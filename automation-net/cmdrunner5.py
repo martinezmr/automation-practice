347892f0078093cc0141e0cc0cfbb83ebcfb90d1
Manny Martinez <mmartinez1513@gmail.com>
2022-05-16 10:30:44 -0400

    getpass

---
+++ automation-net/cmdrunner5.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from getpass import getpass
+import netmiko
+import json
+#import devices from devices.json
+
+def get_input(prompt=''):
+    try:
+        line = raw_input(prompt)
+    except NameError:
+        line = input(prompt)
+    return line
+
+def get_credentials():
+    #Prompts for, and returns, a username and password.
+    username = get_input('Enter Username: ')
+    password = None
+    while not password:
+        password = getpass()
+        password_verify = getpass('Retype your password: ')
+        if password != password_verify:
+            print('Passwords do not match.  Try again.')
+            password = None
+    return username, password
+
+netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException, netmiko.ssh_exception.NetMikoAuthenticationException)
+
+usrname, password = get_credentials()
+
+with open('devices.json') as dev_file:
+    devices = json.load(dev_file)
+
+for device in devices:
+    device['username'] = username
+    device['password'] = password
+    try:
+        print('~'*79)
+        print('Connecting to device', device['ip'])
+        connection = netmiko.ConnectHandler(**device)
+        print(connection.send_command('sh ip int status'))
+        connection.disconnect()
+    except netmiko_exceptions as e:
+        print('Failed to ', device['ip'], e)
