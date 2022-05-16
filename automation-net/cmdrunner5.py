d0a5b523af175ab17c1c54e47c628b7ec9c657ed
Manny Martinez <mmartinez1513@gmail.com>
2022-05-16 15:44:33 -0400

    new

--- automation-net/cmdrunner5.py
+++ automation-net/cmdrunner5.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
 import netmiko
 import json
 import mytools
-#import devices from devices.json
+import sys
+import signal
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL) #IOError: Broken pipe
+signal.signal(signal.SIGINT, signal.SIG_DFL) #KeyboardInterrupt: Ctrl-C
+
+
+
+if len(sys.argv) < 3:
+    print('Usage: cmdrunner.py commands.txt devices.json')
+    exit()
 
 netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException, netmiko.ssh_exception.NetMikoAuthenticationException)
 
 username, password = mytools.get_credentials()
 
-with open('devices.json') as dev_file:
+with open(sys.argv[1]) as cmd_file:
+    commands = cmd_file.readlines()
+
+with open(sys.argv[2]) as dev_file:
     devices = json.load(dev_file)
 
 for device in devices:
@@ -18,7 +34,10 @@ for device in devices:
         print('~'*79)
         print('Connecting to device', device['ip'])
         connection = netmiko.ConnectHandler(**device)
-        print(connection.send_command('sh ip int brief'))
+        for command in commands:
+            print('## Output of ' + command)
+            print(connection.send_command(command))
+            print()
         connection.disconnect()
     except netmiko_exceptions as e:
         print('Failed to ', device['ip'], e)
