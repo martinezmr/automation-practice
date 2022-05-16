7e349364b1da9e09a82759207005961569ca4e8a
Manny Martinez <mmartinez1513@gmail.com>
2022-05-16 08:22:55 -0400

    device[ip]

--- automation-net/cmdrunner3.py
+++ automation-net/cmdrunner3.py
@@ -24,9 +24,9 @@ netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException, netmiko.ssh
 for device in devices:
     try:
         print('~'*79)
-        print('Connecting to device', device)
+        print('Connecting to device', device['ip'])
         connection = netmiko.ConnectHandler(**device)
         print(connection.send_command('sh ip int brief'))
         connection.disconnect()
     except netmiko_exceptions as e:
-        print('Failed to ', device, e)
+        print('Failed to ', device['ip'], e)
