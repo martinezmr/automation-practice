33f855bed62c112ab7eb1d4be0c6c4cdd27bca0a
Manny Martinez <mmartinez1513@gmail.com>
2022-05-16 08:14:22 -0400

    change 3

--- automation-net/cmdrunner2.py
+++ automation-net/cmdrunner2.py
@@ -12,17 +12,14 @@ device_type = 'cisco_ios'
 username = 'cisco'
 password = 'cisco'
 
+netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException, netmiko.ssh_exception.NetmikoAuthenticationException)
+
 for device in devices:
     try:
         print('~'*79)
-        print('Connecting to device ', device)
+        print('Connecting to device', device)
         connection = netmiko.ConnectHandler(ip=device, device_type=device_type, username=username, password=password)
         print(connection.send_command('sh ip int brief'))
         connection.disconnect()
-    except netmiko.ssh_exception.NetMikoAuthenticationException:
-        print('autnentication failed to ', device)
-#connects to device
-#connection
-#prints the command
-
-#disconnects from device
+    except netmiko_exceptions as e:
+        print('Failed to ', device, e)
