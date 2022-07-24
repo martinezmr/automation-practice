17a3d70c14c843d0c7dac690db4b85fc40f808ba
Manny Martinez <mmartinez1513@gmail.com>
2022-07-24 12:34:56 -0400

    pprint

--- advanced-networks/napalmdemo.py
+++ advanced-networks/napalmdemo.py
@@ -1,11 +1,13 @@
+import imp
 import napalm
 import json
+from pprint import pprint
 
 driver = napalm.get_network_driver('ios')
 
 device = driver(hostname='192.168.73.159', username='cisco', password='cisco')
 
 device.open()
-print(device.get_config(retrieve='running', full=False))
-print(device.get_environment())
+pprint(device.get_config(retrieve='running', full=False))
+pprint(device.get_environment())
 device.close()
