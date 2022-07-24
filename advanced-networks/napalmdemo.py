75f0dd977bbd62d09ec65dc9fdfd6893d0c8108f
Manny Martinez <mmartinez1513@gmail.com>
2022-07-24 12:33:58 -0400

    typo

--- advanced-networks/napalmdemo.py
+++ advanced-networks/napalmdemo.py
@@ -6,6 +6,6 @@ driver = napalm.get_network_driver('ios')
 device = driver(hostname='192.168.73.159', username='cisco', password='cisco')
 
 device.open()
-print(device.getconfig(retrieve='running', full=False))
+print(device.get_config(retrieve='running', full=False))
 print(device.get_environment())
 device.close()
